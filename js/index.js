fa73ab03d6781357a6766aeb627e2fb270fb7bc8
Raghav Sharma <raghavsyt@gmail.com>
2018-05-04 12:54:49 +0530

    added a color

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ document.getElementById('whiteButton').onclick = switchWhite;
 document.getElementById('blueButton').onclick = switchBlue;
 document.getElementById('yellowButton').onclick = switchYellow;
 document.getElementById('redButton').onclick = switchRed;
+document.getElementById('orangeButton').onclick = switchOrange;
 
 function switchGray() {
   document.getElementsByTagName('body')[0].style.backgroundColor = 'gray'; 
@@ -18,11 +19,15 @@ function switchBlue() {
   document.getElementsByTagName('body')[0].style.backgroundColor = 'blue'; 
   document.getElementsByTagName('body')[0].style.color = 'white'; 
 }
-
+function switchOrange() {
+  document.getElementsByTagName('body')[0].style.backgroundColor = 'orange'; 
+  document.getElementsByTagName('body')[0].style.color = 'black'; 
+}
 function switchYellow() {
   document.getElementsByTagName('body')[0].style.backgroundColor = 'yellow'; 
   document.getElementsByTagName('body')[0].style.color = 'black'; 
 }
+
 function switchRed() {
   document.getElementsByTagName('body')[0].style.backgroundColor = 'tomato'; 
   document.getElementsByTagName('body')[0].style.color = 'white'; 
